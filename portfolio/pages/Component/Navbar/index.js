58d8dd76fe49e7356653f4f6b4e57b9ed22c8a01
Prem R Pattanayak <prem.p.in@rupeek.com>
2024-02-16 12:00:49 +0530

    adding style global

---
+++ portfolio/pages/Component/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    mainWrapper: {
+        
+    }
+});
+
+export default function index() {
+    const classes = useStyles();
+
+  return (
+    <div>
+
+    </div>
+  )
+}
--- portfolio/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>Hi, Next.js!</h1>
-  }
